b70ddb584852c6e0b1112caf5c15780eb23e0153
ivoroberto92 <56032688+ivoroberto92@users.noreply.github.com>
2021-07-12 19:37:25 -0300

    Primeiro commit

---
+++ src/main/java/br/com/devVagas/devVagas/DevVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.devVagas.devVagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevVagasApplication.class, args);
+	}
+
+}
