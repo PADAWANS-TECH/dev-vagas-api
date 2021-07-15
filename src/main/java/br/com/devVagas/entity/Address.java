2352e2fbf2e7e9d3d1fab549f23dd9d5d8d1ba12
GustavoHenrique18n <gustavohenriquedoespirito@gmail.com>
2021-07-15 15:55:02 -0300

    fix(Adress.java): fix getter and setter

--- src/main/java/br/com/devVagas/entity/Address.java
+++ src/main/java/br/com/devVagas/entity/Address.java
@@ -7,21 +7,22 @@ import java.util.List;
 @Entity
 @Table(name="ENDERECO")
 @NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
 @ToString(exclude="id")
 public class Address {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="ID" , nullable = false)
-    @Getter private Long id;
+    private Long id;
     @Column(name="LOGRADOURO", nullable = false)
-    @Getter @Setter private String publicPlace;
+    private String publicPlace;
     @Column(name= "BAIRRO" , nullable = false)
-    @Getter @Setter private String neighborhood;
+     private String neighborhood;
     @Column(name= "NUMERO" , nullable = false)
-    @Getter @Setter private String number;
+     private String number;
     @Column(name="COMPLEMENTO", nullable = true)
-    @Getter @Setter private String complement;
+     private String complement;
     @Column(name="CEP", nullable= false)
-    @Getter @Setter private String cep;
+     private String cep;
     // @ManyToOne
     // @Getter @Setter private City city;
 }
