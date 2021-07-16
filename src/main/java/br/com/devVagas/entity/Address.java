cc7558d84720b293dc45cf4ee24a335136f66d30
ivoroberto92 <56032688+ivoroberto92@users.noreply.github.com>
2021-07-16 19:40:58 -0300

    Corrigindo bug de conexão com o banco de dados

--- src/main/java/br/com/devVagas/entity/Address.java
+++ src/main/java/br/com/devVagas/entity/Address.java
@@ -4,25 +4,33 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.List;
 
-@Entity
-@Table(name="ENDERECO")
+
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter
 @ToString(exclude="id")
+@Entity
+@Table(name="TB_ENDERECO")
 public class Address {
+	
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="ID" , nullable = false)
     private Long id;
+    
     @Column(name="LOGRADOURO", nullable = false)
     private String publicPlace;
+    
     @Column(name= "BAIRRO" , nullable = false)
      private String neighborhood;
+    
     @Column(name= "NUMERO" , nullable = false)
      private String number;
+    
     @Column(name="COMPLEMENTO", nullable = true)
      private String complement;
+    
     @Column(name="CEP", nullable= false)
      private String cep;
+    
     // @ManyToOne
     // @Getter @Setter private City city;
 }
