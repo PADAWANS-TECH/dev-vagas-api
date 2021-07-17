b81f887d6f448e753af6ddab8ea743d41ccef4d3
GustavoHenrique18n <gustavohenriquedoespirito@gmail.com>
2021-07-17 00:41:36 -0300

    feat: skill Class

--- src/main/java/br/com/devVagas/entity/Skill.java
+++ src/main/java/br/com/devVagas/entity/Skill.java
@@ -3,25 +3,33 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Table(name="SKILL")
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(exclude="id")
 public class Skill {
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="ID" , nullable = false)
-    @Getter private Long id;
+    private Long id;
     @Column(name="NOME" , nullable = false)
-    @Getter @Setter private String name;
+    private String name;
     @Column(name="COR" , nullable = false)
-    @Getter @Setter private String color;
+    private String color;
     @Column(name="NIVEL_CONHECIMENTO", nullable = false)
-    @Getter @Setter private Integer knowledgeLevel;
+    private Integer knowledgeLevel;
     @Column(name="DT_ATUALIZACAO")
-    @Getter @Setter private LocalDateTime exclusionDate;
+    private LocalDateTime exclusionDate;
     @Column(name="DT_CRIACAO")
-    @Getter @Setter private LocalDateTime createDate;
+    private LocalDateTime createDate;
     @Column(name="DT_EXCLUSAO")
-    @Getter @Setter private LocalDateTime updateDate;
+    private LocalDateTime updateDate;
+    //@ManyToOne
+    // private Analyst createAnalyst
+    // private Analist exclusionAnalyst
+    // private Analist updateAnalyst
 }
