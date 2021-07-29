742f3da431f8c3eb4ad73269080bc7f0af8ec9ab
deniswliborges <deniswliborges@gmail.com>
2021-07-28 21:51:17 -0300

    Relacionamento Class Analyst.

--- src/main/java/br/com/devVagas/entity/TypeQuestion.java
+++ src/main/java/br/com/devVagas/entity/TypeQuestion.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -43,13 +45,16 @@ public class TypeQuestion {
 	@Column(name = "DT_EXCLUSAO")
 	private LocalDateTime excluysionDate;
 	
-	//@Column(name = "ID_ANALISTA_ATUALIZACAO")
-	//private Analyst updateAnalysts;
+	@Column(name = "ID_ANALISTA_ATUALIZACAO")
+	private Analyst updateAnalysts;
 	
-	//@Column(name = "ID_ANALISTA_CRIACAO")
-	//private Analyst createAnalysts;
+	@Column(name = "ID_ANALISTA_CRIACAO")
+	private Analyst createAnalysts;
 	
-	//@Column(name = "ID_ANALISTA_EXCLUSAO")
-	//private Analyst exclusionAnalysts;	
+	@Column(name = "ID_ANALISTA_EXCLUSAO")
+	private Analyst exclusionAnalysts;	
 	
+	@ManyToOne
+	@JoinColumn(name = "analyst_id")
+	private Analyst analyst;
 }
