64273abb01f7afadf908a9585e065824471ed74f
ivoroberto92 <56032688+ivoroberto92@users.noreply.github.com>
2021-07-31 00:20:57 -0300

    Inserindo relacionamento N:N entre as classes SelectionProcess e Stage

--- src/main/java/br/com/devVagas/entity/SelectionProcess.java
+++ src/main/java/br/com/devVagas/entity/SelectionProcess.java
@@ -1,8 +1,22 @@
 package br.com.devVagas.entity;
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name="PROCESSO_SELETIVO")
@@ -41,5 +55,11 @@ public class SelectionProcess {
     // private Vacancy vacancy
     //@ManyToOne
     // private Analyst analyst
+
+    @ManyToMany
+    @JoinTable(name = "PROCESSO_SELETIVO_ETAPA", joinColumns = 
+    @JoinColumn(name = "processo_seletivo_id", nullable = false), inverseJoinColumns = 
+    @JoinColumn(name = "stage_id", nullable = false))
+    private List<Stage> stages;
 }
 
