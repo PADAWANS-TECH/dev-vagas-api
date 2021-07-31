581f9ea2e8084bcdbaf19cb1a26c30b41960f522
ivoroberto92 <56032688+ivoroberto92@users.noreply.github.com>
2021-07-31 00:27:35 -0300

    Adicionando interface CityRepository

---
+++ src/main/java/br/com/devVagas/repository/CityRepository.java
@@ -0,0 +1,11 @@
+package br.com.devVagas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.devVagas.entity.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long>{
+
+}
