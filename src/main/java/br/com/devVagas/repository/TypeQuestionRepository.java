828a1f4aa4d11f81d348fbe8a53d8a031027f264
ivoroberto92 <56032688+ivoroberto92@users.noreply.github.com>
2021-07-31 00:35:03 -0300

    Adicionando interface TypeQuestionRepository

---
+++ src/main/java/br/com/devVagas/repository/TypeQuestionRepository.java
@@ -0,0 +1,9 @@
+package br.com.devVagas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TypeQuestionRepository extends JpaRepository<TypeQuestion, Long>{
+
+}
