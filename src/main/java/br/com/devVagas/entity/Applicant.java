6535e28cea3d4431022c04e10daac62c084a911a
deniswliborges <deniswliborges@gmail.com>
2021-08-02 20:23:24 -0300

    Correcao Mapeamento Skill e Candidato

--- src/main/java/br/com/devVagas/entity/Applicant.java
+++ src/main/java/br/com/devVagas/entity/Applicant.java
@@ -13,14 +13,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -106,11 +102,10 @@ public class Applicant {
 	private Analyst exclusionAnalyst;
 	
 	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "endereco_id",referencedColumnName = "id")
+	@JoinColumn(name = "ENDERECO_ID",referencedColumnName = "id")
 	private List<Address> address = new ArrayList<>();
 	
-	@ManyToMany
-	//falta realizar o mapeamento correto
+	@ManyToMany(mappedBy = "applicanties")
 	private List<Skill> skills = new ArrayList<>();	
 	
 	@ManyToMany(mappedBy = "applicanties",fetch = FetchType.LAZY)
--- src/main/java/br/com/devVagas/entity/Skill.java
+++ src/main/java/br/com/devVagas/entity/Skill.java
@@ -1,8 +1,23 @@
 package br.com.devVagas.entity;
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name="TB_SKILL")
@@ -42,5 +57,9 @@ public class Skill {
 //    private Analist exclusionAnalyst
 //    private Analist modifyAnalyst
 //}
-    
+    @ManyToMany
+    @JoinTable(name = "TB_CANDIDATO_SKILL", joinColumns = 
+    @JoinColumn(name = "skill_id"), inverseJoinColumns = 
+    @JoinColumn(name = "applicant_id"))
+    private List<Applicant> applicanties = new ArrayList<>();
 }
