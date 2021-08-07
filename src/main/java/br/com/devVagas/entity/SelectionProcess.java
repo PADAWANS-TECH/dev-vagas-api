e315c7b9c701d8e24e63883a0d26729bfae90371
Arthur Flausino dos Santos <arthurflausino@hotmail.com>
2021-08-07 09:35:47 -0300

    Adicionando relacionamento entre SelectionProcess e Skills

--- src/main/java/br/com/devVagas/entity/SelectionProcess.java
+++ src/main/java/br/com/devVagas/entity/SelectionProcess.java
@@ -3,8 +3,10 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,6 +14,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -90,5 +93,8 @@ public class SelectionProcess {
     @JoinColumn(name = "selectionProcess_id"), inverseJoinColumns = 
     @JoinColumn(name = "applicant_id"))
     private List<Applicant> applicanties = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "selectionProcess",cascade = CascadeType.PERSIST)
+	private List<Skill>skills = new ArrayList<>();
 }
-
+	
