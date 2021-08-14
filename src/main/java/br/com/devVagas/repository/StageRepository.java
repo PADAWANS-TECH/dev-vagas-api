b51497e5d91da868e742a962215a0370a494cba5
Arthur Flausino dos Santos <arthurflausino@hotmail.com>
2021-08-14 11:02:41 -0300

    Corrigindo repository Stage

--- src/main/java/br/com/devVagas/repository/StageRepository.java
+++ src/main/java/br/com/devVagas/repository/StageRepository.java
@@ -3,9 +3,9 @@ package br.com.devVagas.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.devVagas.entity.Company;
+import br.com.devVagas.entity.Stage;
 
 @Repository
-public interface StageRepository extends JpaRepository<Company, Long>{
+public interface StageRepository extends JpaRepository<Stage, Long>{
 
 }
