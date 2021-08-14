75d447c7ae7eb7282fcad25e50c7b4b7fe2f3415
Arthur Flausino dos Santos <arthurflausino@hotmail.com>
2021-08-14 11:04:10 -0300

    Corrigindo repository Quiz

--- src/main/java/br/com/devVagas/repository/QuizRepository.java
+++ src/main/java/br/com/devVagas/repository/QuizRepository.java
@@ -3,9 +3,9 @@ package br.com.devVagas.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.devVagas.entity.Company;
+import br.com.devVagas.entity.Quiz;
 
 @Repository
-public interface QuizRepository extends JpaRepository<Company, Long>{
+public interface QuizRepository extends JpaRepository<Quiz, Long>{
 
 }
