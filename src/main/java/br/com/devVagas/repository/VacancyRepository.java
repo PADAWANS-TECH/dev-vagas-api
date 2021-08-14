b286e99408002225cdbf0c5b707fcb85f0458075
Arthur Flausino dos Santos <arthurflausino@hotmail.com>
2021-08-14 10:55:47 -0300

    Corrigindo repository Vacancy

--- src/main/java/br/com/devVagas/repository/VacancyRepository.java
+++ src/main/java/br/com/devVagas/repository/VacancyRepository.java
@@ -3,9 +3,9 @@ package br.com.devVagas.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.devVagas.entity.Company;
+import br.com.devVagas.entity.Vacancy;
 
 @Repository
-public interface VacancyRepository extends JpaRepository<Company, Long>{
+public interface VacancyRepository extends JpaRepository<Vacancy, Long>{
 
 }
